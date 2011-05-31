58e3db732b756885b46e59ada0215168440eb887
Matteo Panella <morpheus@level28.org>
2011-05-31 02:41:18 +0200

    Feeds!

--- _plugins/helpers.rb
+++ _plugins/helpers.rb
@@ -1,3 +1,5 @@
+require 'nokogiri'
+
 module Liquid
 
   module ExtendedFilters
@@ -19,6 +21,20 @@ module Liquid
       end
     end
 
+    def fix_math(t)
+      t.gsub(/(<script type="math\/tex">)(.*?)(<\/script>)/m, '\( \2 \)').gsub(/(<script type="math\/tex; mode=display">\n)(.*?)(<\/script>)/m, "$$\n\\2$$")
+    end
+
+    def rss_preview(text, delimiter = '<!-- end_preview -->')
+      if text.index(delimiter) != nil
+        doc = Nokogiri::HTML::DocumentFragment.parse text.split(delimiter)[0]
+        rt = doc.inner_text
+      else
+        rt = html_truncatewords(:text => text, :ellipsis => '[...]', :max_length => 300)
+      end
+      rt.gsub(/\n/, ' ').gsub(/\s{2,}/, ' ')
+    end
+
     def escape_javascript(javascript)
       (javascript || '').gsub('\\', '\0\0').gsub('</', '<\/').gsub(/\r\n|\n|\r/, "\\n").gsub(/["']/) { |m| "\\#{m}" }
     end
@@ -27,6 +43,17 @@ module Liquid
       catlinks = []
       categories.each { |cat| catlinks << "<a href='/category/#{slugify(cat)}'>#{cat}</a>" }
       catlinks.join(', ')
+    end
+
+    def html_truncatewords(params)
+      text = params[:text] || raise("text parameter is required")
+      max_length = params[:max_length] || 200
+      ellipsis = params[:ellipsis] || ""
+      ellipsis_length = ellipsis.length
+      doc = Nokogiri::HTML::DocumentFragment.parse text
+      content_length = doc.inner_text.length
+      actual_length = max_length - ellipsis_length
+      content_length > actual_length ? doc.truncate(actual_length).inner_text + ellipsis : doc.inner_text
     end
   end
 
@@ -42,6 +69,34 @@ module Liquid
       end
     end
   end
+
+  module NokogiriTruncator
+    module NodeWithChildren
+      def truncate(max_length)
+        return self if inner_text.length <= max_length
+        truncated_node = self.dup
+        truncated_node.children.remove
+
+        self.children.each do |node|
+          remaining_length = max_length - truncated_node.inner_text.length
+          break if remaining_length <= 0
+          truncated_node.add_child node.truncate(remaining_length)
+        end
+        truncated_node
+      end
+    end
+
+    module TextNode
+      def truncate(max_length)
+        Nokogiri::XML::Text.new(content[0..(max_length - 1)], parent)
+      end
+    end
+  end
+
+  # Register nokogiri extensions
+  Nokogiri::HTML::DocumentFragment.send(:include, NokogiriTruncator::NodeWithChildren)
+  Nokogiri::XML::Element.send(:include, NokogiriTruncator::NodeWithChildren)
+  Nokogiri::XML::Text.send(:include, NokogiriTruncator::TextNode)
 
   # Register filters
   Liquid::Template.register_filter(ExtendedFilters)
